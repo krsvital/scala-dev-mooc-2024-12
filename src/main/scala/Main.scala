1be854b2dcb3c8bfcd902d2183192e433e24a6d4
Vitaliy Shurdesov <sva083@gmail.com>
2025-03-09 11:39:14 +0000

    homework_6_scala3

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
-object Main extends  App {
+import CSV._
 
+object Main extends  App {
+    case class Car(name: String, id: Int, cost: Double, discount: Boolean)
+
+    val csv = 
+        """
+            ferarri;1;100000.00;true
+            lada;2;2000.00;false
+            toyota;3;20000.20;false
+        """
+
+    given delim: Delimiter = Delimiter(";")
+
+    CSV[Car](csv).foreach(println)
 }
